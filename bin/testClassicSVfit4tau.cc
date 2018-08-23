e08154756104398085bcc1ab9ffe5fd8997e93d0
Christian Veelken <christian.veelken@cern.ch>
2018-08-23 14:49:45 +0300

    added VAMP integration to test example

--- bin/testClassicSVfit4tau.cc
+++ bin/testClassicSVfit4tau.cc
@@ -45,41 +45,36 @@ int main(int argc, char* argv[])
   */
 
   int verbosity = 1;
-  //ClassicSVfit4tau svFitAlgo(ClassicSVfit4tau::kAlgoMarkovChain, verbosity);
-  ClassicSVfit4tau svFitAlgo(ClassicSVfit4tau::kAlgoVAMP, verbosity);
+  ClassicSVfit4tau svFitAlgo_MarkovChain(ClassicSVfit4tau::kAlgoMarkovChain, verbosity);
 #ifdef USE_SVFITTF
   //HadTauTFCrystalBall2* hadTauTF = new HadTauTFCrystalBall2();
-  //svFitAlgo.setHadTauTF(hadTauTF);
-  //svFitAlgo.enableHadTauTF();
+  //svFitAlgo_MarkovChain.setHadTauTF(hadTauTF);
+  //svFitAlgo_MarkovChain.enableHadTauTF();
 #endif
   // run with mass constraint for each tau pair to match measured mass (125.06 GeV) of SM-like Higgs boson   
   double massContraint = 125.06;
   //double kappa = 6.; // kappa parameter for log(M) term, cf. Eq. (41) in Nucl.Instrum.Meth. A862 (2017) 54-84
   double kappa = 0.; 
   if ( kappa > 0. ) {
-    svFitAlgo.addLogM_fixed(true, kappa);
+    svFitAlgo_MarkovChain.addLogM_fixed(true, kappa);
   } else {
-    svFitAlgo.addLogM_fixed(false);
+    svFitAlgo_MarkovChain.addLogM_fixed(false);
   }
-  //svFitAlgo.addLogM_dynamic(true, "(m/1000.)*15.");
-  //svFitAlgo.setMaxObjFunctionCalls(100000); // CV: default is 100000 evaluations of integrand per event
 
-  std::cout << "\n\nTesting integration with ditau mass constraint set to " << massContraint << std::endl;
-  svFitAlgo.setLikelihoodFileName("testClassicSVfit4tau_withMassContraint.root");
-  svFitAlgo.setDiTau1MassConstraint(massContraint);
-  svFitAlgo.setDiTau2MassConstraint(massContraint);  
-  svFitAlgo.integrate(measuredTauLeptons, measuredMETx, measuredMETy, covMET);
-assert(0); // !!! ONLY FOR TESTING !!!
-  bool isValidSolution_1stRun = svFitAlgo.isValidSolution();
-  double dihiggs_mass_1stRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo.getHistogramAdapter())->getMass();
-  double dihiggs_massErr_1stRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo.getHistogramAdapter())->getMassErr();
-  double dihiggs_transverseMass_1stRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo.getHistogramAdapter())->getTransverseMass();
-  double dihiggs_transverseMassErr_1stRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo.getHistogramAdapter())->getTransverseMassErr();
-  double ditau1_mass_1stRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo.getHistogramAdapter())->ditau1()->getMass();
-  double ditau1_massErr_1stRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo.getHistogramAdapter())->ditau1()->getMassErr();
-  double ditau2_mass_1stRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo.getHistogramAdapter())->ditau2()->getMass();
-  double ditau2_massErr_1stRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo.getHistogramAdapter())->ditau2()->getMassErr();
-
+  std::cout << "\n\nTesting Markov-Chain integration with ditau mass constraint set to " << massContraint << std::endl;
+  svFitAlgo_MarkovChain.setLikelihoodFileName("testClassicSVfit4tau_withMassContraint.root");
+  svFitAlgo_MarkovChain.setDiTau1MassConstraint(massContraint);
+  svFitAlgo_MarkovChain.setDiTau2MassConstraint(massContraint);  
+  svFitAlgo_MarkovChain.integrate(measuredTauLeptons, measuredMETx, measuredMETy, covMET);
+  bool isValidSolution_1stRun = svFitAlgo_MarkovChain.isValidSolution();
+  double dihiggs_mass_1stRun = svFitAlgo_MarkovChain.getMass();
+  double dihiggs_massErr_1stRun = svFitAlgo_MarkovChain.getMassErr();
+  double dihiggs_transverseMass_1stRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo_MarkovChain.getHistogramAdapter())->getTransverseMass();
+  double dihiggs_transverseMassErr_1stRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo_MarkovChain.getHistogramAdapter())->getTransverseMassErr();
+  double ditau1_mass_1stRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo_MarkovChain.getHistogramAdapter())->ditau1()->getMass();
+  double ditau1_massErr_1stRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo_MarkovChain.getHistogramAdapter())->ditau1()->getMassErr();
+  double ditau2_mass_1stRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo_MarkovChain.getHistogramAdapter())->ditau2()->getMass();
+  double ditau2_massErr_1stRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo_MarkovChain.getHistogramAdapter())->ditau2()->getMassErr();
   if ( isValidSolution_1stRun ) {
     std::cout << "found valid solution: mass = " << dihiggs_mass_1stRun << " +/- " << dihiggs_massErr_1stRun << " (expected value = 405.091 +/- 10.6929),"
               << " transverse mass = " << dihiggs_transverseMass_1stRun << " +/- " << dihiggs_transverseMassErr_1stRun << " (expected value = 361.319 +/- 12.8636)" << std::endl;
@@ -89,7 +84,7 @@ assert(0); // !!! ONLY FOR TESTING !!!
     std::cout << "sorry, failed to find valid solution !!" << std::endl;
   }
   if ( kappa == 0. ) {
-    if (std::abs((dihiggs_mass_1stRun - 405.091) / 415.218) > 0.001) return 1;
+    if (std::abs((dihiggs_mass_1stRun - 405.091) / 405.091) > 0.001) return 1;
     if (std::abs((dihiggs_massErr_1stRun - 10.6929) / 10.6929) > 0.001) return 1;
     if (std::abs((dihiggs_transverseMass_1stRun - 361.319) / 361.319) > 0.001) return 1;
     if (std::abs((dihiggs_transverseMassErr_1stRun - 12.8636) / 12.8636) > 0.001) return 1;
@@ -98,21 +93,20 @@ assert(0); // !!! ONLY FOR TESTING !!!
   // re-run without mass constraint for each tau pair;
   // this mode will set the mass of the second tau pair to match the mass of the first tau pair,
   // while the mass of the first tau pair is allowed to freely vary within the fit
-  std::cout << "\n\nTesting integration without fixed ditau mass constraint" << std::endl;
-  svFitAlgo.setLikelihoodFileName("testClassicSVfit4tau.root");
-  svFitAlgo.setDiTau1MassConstraint(-1.);
-  svFitAlgo.setDiTau2MassConstraint(-1.);
-  svFitAlgo.integrate(measuredTauLeptons, measuredMETx, measuredMETy, covMET);
-  bool isValidSolution_2ndRun = svFitAlgo.isValidSolution();
-  double dihiggs_mass_2ndRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo.getHistogramAdapter())->getMass();
-  double dihiggs_massErr_2ndRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo.getHistogramAdapter())->getMassErr();
-  double dihiggs_transverseMass_2ndRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo.getHistogramAdapter())->getTransverseMass();
-  double dihiggs_transverseMassErr_2ndRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo.getHistogramAdapter())->getTransverseMassErr();
-  double ditau1_mass_2ndRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo.getHistogramAdapter())->ditau1()->getMass();
-  double ditau1_massErr_2ndRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo.getHistogramAdapter())->ditau1()->getMassErr();
-  double ditau2_mass_2ndRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo.getHistogramAdapter())->ditau2()->getMass();
-  double ditau2_massErr_2ndRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo.getHistogramAdapter())->ditau2()->getMassErr();
-
+  std::cout << "\n\nTesting Markov-Chain integration without fixed ditau mass constraint" << std::endl;
+  svFitAlgo_MarkovChain.setLikelihoodFileName("testClassicSVfit4tau.root");
+  svFitAlgo_MarkovChain.setDiTau1MassConstraint(-1.);
+  svFitAlgo_MarkovChain.setDiTau2MassConstraint(-1.);
+  svFitAlgo_MarkovChain.integrate(measuredTauLeptons, measuredMETx, measuredMETy, covMET);
+  bool isValidSolution_2ndRun = svFitAlgo_MarkovChain.isValidSolution();
+  double dihiggs_mass_2ndRun = svFitAlgo_MarkovChain.getMass();
+  double dihiggs_massErr_2ndRun = svFitAlgo_MarkovChain.getMassErr();
+  double dihiggs_transverseMass_2ndRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo_MarkovChain.getHistogramAdapter())->getTransverseMass();
+  double dihiggs_transverseMassErr_2ndRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo_MarkovChain.getHistogramAdapter())->getTransverseMassErr();
+  double ditau1_mass_2ndRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo_MarkovChain.getHistogramAdapter())->ditau1()->getMass();
+  double ditau1_massErr_2ndRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo_MarkovChain.getHistogramAdapter())->ditau1()->getMassErr();
+  double ditau2_mass_2ndRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo_MarkovChain.getHistogramAdapter())->ditau2()->getMass();
+  double ditau2_massErr_2ndRun = static_cast<HistogramAdapterDiHiggs*>(svFitAlgo_MarkovChain.getHistogramAdapter())->ditau2()->getMassErr();
   if ( isValidSolution_2ndRun ) {
     std::cout << "found valid solution: mass = " << dihiggs_mass_2ndRun << " +/- " << dihiggs_massErr_2ndRun << " (expected value = 366.992 +/- 88.1767),"
               << " transverse mass = " << dihiggs_transverseMass_2ndRun << " +/- " << dihiggs_transverseMassErr_2ndRun << " (expected value = 343.908 +/- 71.5488)" << std::endl;
@@ -126,6 +120,35 @@ assert(0); // !!! ONLY FOR TESTING !!!
     if (std::abs((dihiggs_massErr_2ndRun - 88.1767) / 88.1767) > 0.001) return 1;
     if (std::abs((dihiggs_transverseMass_2ndRun - 343.908) / 343.908) > 0.001) return 1;
     if (std::abs((dihiggs_transverseMassErr_2ndRun - 71.5488) / 71.5488) > 0.001) return 1;
+  }
+
+  ClassicSVfit4tau svFitAlgo_VAMP(ClassicSVfit4tau::kAlgoVAMP, verbosity);
+#ifdef USE_SVFITTF
+  //HadTauTFCrystalBall2* hadTauTF = new HadTauTFCrystalBall2();
+  //svFitAlgo_VAMP.setHadTauTF(hadTauTF);
+  //svFitAlgo_VAMP.enableHadTauTF();
+#endif
+  if ( kappa > 0. ) {
+    svFitAlgo_VAMP.addLogM_fixed(true, kappa);
+  } else {
+    svFitAlgo_VAMP.addLogM_fixed(false);
+  }
+
+  std::cout << "\n\nTesting VAMP integration with ditau mass constraint set to " << massContraint << std::endl;
+  svFitAlgo_VAMP.setDiTau1MassConstraint(massContraint);
+  svFitAlgo_VAMP.setDiTau2MassConstraint(massContraint);  
+  svFitAlgo_VAMP.integrate(measuredTauLeptons, measuredMETx, measuredMETy, covMET);
+  bool isValidSolution_3rdRun = svFitAlgo_VAMP.isValidSolution();
+  double dihiggs_mass_3rdRun = svFitAlgo_VAMP.getMass();
+  double dihiggs_massErr_3rdRun  = svFitAlgo_VAMP.getMassErr();
+  if ( isValidSolution_3rdRun ) {
+    std::cout << "found valid solution: mass = " << dihiggs_mass_3rdRun << " +/- " << dihiggs_massErr_3rdRun << " (expected value = 404.853 +/- 10.1509)" << std::endl;
+  } else {
+    std::cout << "sorry, failed to find valid solution !!" << std::endl;
+  }
+  if ( kappa == 0. ) {
+    if (std::abs((dihiggs_mass_3rdRun - 404.853) / 404.853) > 0.001) return 1;
+    if (std::abs((dihiggs_massErr_3rdRun - 10.1509) / 10.1509) > 0.001) return 1;
   }
 
   return 0;
